d80dd8f098835055345f0097bea643c633302e4e
Krishna47ak <anandakrishna720@gmail.com>
2023-02-11 19:41:04 +0530

    Created auth, user and profile modals and routes

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,10 +1,24 @@
 const express = require('express')
+const connectDB = require('./config/db')
 
 const app = express()
+
+// Connect Database
+connectDB()
+
+// Init middlewares
+app.use(express.json({ extended: false }))
+
+// Define Routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/posts', require('./routes/api/posts'))
+
+app.get('/', (req, res) => res.send('Api Running'))
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log('Server started'))
 
-app.get('/', (req, res) => res.send('Api Running'))
 
