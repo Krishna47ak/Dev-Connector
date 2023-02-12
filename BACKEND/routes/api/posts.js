bb208a1d2a6f21bd0e5ffe7f8f9e8b1907c1d8f5
Krishna47ak <anandakrishna720@gmail.com>
2023-02-13 00:47:55 +0530

    Created endpoint to delete a user's post

--- BACKEND/routes/api/posts.js
+++ BACKEND/routes/api/posts.js
@@ -59,4 +59,30 @@ router.get('/:id', auth, async (req, res) => {
     }
 })
 
+
+// To delete a user's post
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            return res.status(404).json({ msg: "Posts not found" })
+        }
+
+        // Check user
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: "User not authorized" })
+        }
+
+        await post.remove()
+        res.json({ msg: "Post removed" })
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: "Posts not found" })
+        }
+        res.status(500).send('Server Error')
+    }
+})
+
+
 module.exports = router
