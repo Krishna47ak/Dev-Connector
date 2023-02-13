a88e60995cabc487858e9e8d7bfa38ae2d56526d
Krishna47ak <anandakrishna720@gmail.com>
2023-02-13 22:30:28 +0530

    Created endpoint for adding comments

--- BACKEND/routes/api/posts.js
+++ BACKEND/routes/api/posts.js
@@ -137,5 +137,35 @@ router.put('/unlike/:id', auth, async (req, res) => {
     }
 })
 
+// To create comments
+router.post('/comment/:id', auth, [check('text', 'Text is required').not().isEmpty()], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        const post = await Post.findById(req.params.id)
+
+        const newComment = {
+            user: req.user.id,
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar
+        }
+        post.comments.unshift(newComment)
+
+        await post.save()
+        res.json(post.comments)
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: "Posts not found" })
+        }
+        res.status(500).send('Server Error')
+    }
+
+})
 
 module.exports = router
