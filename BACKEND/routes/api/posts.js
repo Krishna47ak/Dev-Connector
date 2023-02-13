b20f92908efb2d8e451ecc4a3bbb9008312a93f1
Krishna47ak <anandakrishna720@gmail.com>
2023-02-13 21:15:45 +0530

    Created endpoint to like a post

--- BACKEND/routes/api/posts.js
+++ BACKEND/routes/api/posts.js
@@ -84,5 +84,31 @@ router.delete('/:id', auth, async (req, res) => {
     }
 })
 
+// To like a post
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        if (!post) {
+            return res.status(404).json({ msg: "Posts not found" })
+        }
+
+        // Check if the post has already been liked
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.status(400).json({ msg: 'Post already liked' })
+        }
+
+        post.likes.unshift({ user: req.user.id })
+
+        await post.save()
+        res.json(post.likes)
+    } catch (err) {
+        console.error(err.message);
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: "Posts not found" })
+        }
+        res.status(500).send('Server Error')
+    }
+})
+
 
 module.exports = router
