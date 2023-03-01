388f00669a6c3e16c2a6dd81286890d2811282e2
Krishna47ak <anandakrishna720@gmail.com>
2023-03-01 21:59:51 +0530

    Created post screen and its components

--- FRONTEND/client/src/components/posts/Posts.js
+++ FRONTEND/client/src/components/posts/Posts.js
@@ -2,17 +2,36 @@ import { Fragment, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getPosts } from '../../store/actions/post'
+import Spinner from '../layout/Spinner'
+import PostItem from './PostItem'
+import Alert from '../layout/Alert'
+import PostForm from './PostForm'
 
-const Posts = ({ post: { posts, loading } , getPosts }) => {
+const Posts = ({ post: { posts, loading }, getPosts }) => {
     useEffect(() => {
-      getPosts()
-    
-    }, [getPosts])
-    
+        getPosts()
 
-  return (
-    <div>Posts</div>
-  )
+    }, [getPosts])
+
+
+    return loading ? <Spinner /> : <Fragment>
+        <section className="container">
+            <Alert/>
+            <h1 className="large text-primary">
+                Posts
+            </h1>
+            <p className="lead"><i className="fas fa-user"></i> Welcome to the community!</p>
+
+            <PostForm />
+
+            <div className="posts">
+                {posts.map(post => (
+                    <PostItem key={post._id} post={post} />
+                ))}
+            </div>
+        </section>
+    </Fragment>
+
 }
 
 Posts.propTypes = {
--- FRONTEND/client/src/store/actions/post.js
+++ FRONTEND/client/src/store/actions/post.js
@@ -1,12 +1,60 @@
 import axios from "axios";
 import { setAlert } from "./alert";
-import { GET_POSTS, POST_ERROR } from "./types";
+import { ADD_POST, DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from "./types";
 
 export const getPosts = () => async dispatch => {
     try {
         const response = await axios.get('/api/posts')
 
         dispatch({ type: GET_POSTS, payload: response.data })
+    } catch (err) {
+        dispatch({ type: POST_ERROR, payload: { msg: err.response.statusText, status: err.response.status } })
+    }
+}
+
+export const addLike = (postId) => async dispatch => {
+    try {
+        const response = await axios.put(`/api/posts/like/${postId}`)
+
+        dispatch({ type: UPDATE_LIKES, payload: { postId, likes: response.data } })
+    } catch (err) {
+        dispatch({ type: POST_ERROR, payload: { msg: err.response.statusText, status: err.response.status } })
+    }
+}
+
+export const removeLike = (postId) => async dispatch => {
+    try {
+        const response = await axios.put(`/api/posts/unlike/${postId}`)
+
+        dispatch({ type: UPDATE_LIKES, payload: { postId, likes: response.data } })
+    } catch (err) {
+        dispatch({ type: POST_ERROR, payload: { msg: err.response.statusText, status: err.response.status } })
+    }
+}
+
+export const deletePost = (postId) => async dispatch => {
+    try {
+        await axios.delete(`/api/posts/${postId}`)
+
+        dispatch({ type: DELETE_POST, payload: postId })
+        dispatch(setAlert('Post Removed', 'danger'))
+    } catch (err) {
+        dispatch({ type: POST_ERROR, payload: { msg: err.response.statusText, status: err.response.status } })
+    }
+}
+
+export const addPost = (formData) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+
+    try {
+        const response = await axios.post('/api/posts', formData, config )
+
+        dispatch({ type: ADD_POST, payload: response.data })
+        dispatch(setAlert('Post Created', 'success'))
     } catch (err) {
         dispatch({ type: POST_ERROR, payload: { msg: err.response.statusText, status: err.response.status } })
     }
